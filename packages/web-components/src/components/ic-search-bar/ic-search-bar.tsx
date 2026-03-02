df2dfdb14bbe3c33d1240177c6773c0d6b16b865
jd3267 <228690498+jd3267@users.noreply.github.com>
2026-03-02 17:01:15 +0000

    fix(web-components): fix to ic-button theme colour in ic-select and ic-search-bar
    fix to ic-button theme colour by setting to inherit if not focused in ic-select and ic-search-bar

--- packages/web-components/src/components/ic-search-bar/ic-search-bar.tsx
+++ packages/web-components/src/components/ic-search-bar/ic-search-bar.tsx
@@ -937,7 +937,7 @@ export class SearchBar {
                 onKeyDown={this.handleClear}
                 type={"button"}
                 variant="icon-tertiary"
-                {...(theme !== "inherit" ? { theme } : {})}
+                theme={clearButtonFocused ? "light" : "inherit"}
               />
               <div class="divider"></div>
             </div>
@@ -967,7 +967,7 @@ export class SearchBar {
                 onKeyDown={this.handleSubmitSearchKeyDown}
                 type={searchButtonType}
                 variant="icon-tertiary"
-                {...(theme !== "inherit" ? { theme } : {})}
+                theme={searchSubmitFocused ? "light" : "inherit"}
               />
             </div>
           </ic-input-component-container>
